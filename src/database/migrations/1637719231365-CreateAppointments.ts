148eb1b08d8dcb0fcf0eeb0cdc75d3d00d74e6a6
vanderL <vnd.vander@gmail.com>
2021-12-01 19:04:45 -0300

    Relationship models

--- src/database/migrations/1637719231365-CreateAppointments.ts
+++ src/database/migrations/1637719231365-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1637719231365 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
