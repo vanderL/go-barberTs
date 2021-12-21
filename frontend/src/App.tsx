c31bda50287cc60b8162704a74bf4ba7c816900b
Vander Lima <vnd.vander@gmail.com>
2021-12-21 16:57:15 -0300

    Page SigIn Styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
+import SigIn from './pages/SignIn';
+
 const App: React.FC = () => {
   return (
     <>
-      <h1>Oi</h1>
+      <SigIn />
+      <GlobalStyle />
     </>
   )
 }
