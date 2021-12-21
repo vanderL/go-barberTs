69d2434c0cd32ca90b7620c8a3bdf17d2fac95fa
Vander Lima <vnd.vander@gmail.com>
2021-12-21 17:50:44 -0300

    Refactory: Creating custom components Input and Button

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 import { Container, Background, Content } from './styles'
 
@@ -13,9 +16,9 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
