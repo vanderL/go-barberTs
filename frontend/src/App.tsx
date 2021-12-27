404c951defcb9c21579aa801ff95de086c081694
Vander Lima <vnd.vander@gmail.com>
2021-12-27 20:27:52 -0300

    signOut

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import GlobalStyle from './styles/global';
 import SigIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
   return (
