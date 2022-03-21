0d01f8fc93375b57d6a5581d2ac6aad909c96bcf
Vander Lima <vnd.vander@gmail.com>
2022-03-21 12:23:24 -0300

    Action SignUp

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -4,7 +4,11 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErros from "../../utils/getValidationErros";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+
+import api from "../../services/api";
+
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,15 +17,23 @@ import Button from "../../components/Button";
 
 import { Container, Background, Content, AnimationContainer } from './styles'
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
     try {
       formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
-        nome: Yup.string().required('Nome obrigatório'),
+        name: Yup.string().required('Nome obrigatório'),
         email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
@@ -30,13 +42,29 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
+      await api.post('/users', data);
+
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado!',
+        description: 'Você já pode fazer seu logon no GoBarber!',
+      });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErros(err);
         formRef.current?.setErrors(errors);
+
+        return;
       }
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+      });
     }
-  }, []);
+  }, [addToast, history]);
 
 
   return (
@@ -51,7 +79,7 @@ const SignUp: React.FC = () => {
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu Cadastro</h1>
 
-            <Input name="nome" type="text" icon={FiUser} placeholder="Nome" />
+            <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
             <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
             <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
             <Button type="submit">Cadastrar</Button>
