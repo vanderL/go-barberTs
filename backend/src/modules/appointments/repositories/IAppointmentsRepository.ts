3510494c64a7204eac575b0da412f37c54183c09
Vander Lima <vnd.vander@gmail.com>
2022-03-22 17:34:31 -0300

    Refactoring Repository Appointment

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable semi */
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
