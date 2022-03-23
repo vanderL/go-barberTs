6662021b597ef0b950a4a1e6d2ecbc31563af6fd
Vander Lima <vnd.vander@gmail.com>
2022-03-23 18:59:51 -0300

    fix: Standard controller - index, show, create, update and delete

--- backend/src/modules/users/infra/http/controllers/UsersController.ts
+++ backend/src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -15,17 +14,5 @@ export default class UsersController {
     });
 
     return response.json({ name, email });
-  }
-
-  public async updateAvatar(request: Request, response: Response): Promise<Response> {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id!,
-      avatarFilename: request.file?.filename!,
-    });
-    // delete user.password;
-
-    return response.json(user);
   }
 }
